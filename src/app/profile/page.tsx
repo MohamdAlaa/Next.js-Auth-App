fccc987223575033604b005abeb31fd59c76f225
MohamdAlaa <mohamedalaaelmenyawe606@gmail.com>
2025-08-09 14:57:03 +0300

    -Added logic to pofile page for premium

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,25 @@
+import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  const session = await getSession();
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
   return (
     <div className="profile">
-      <h1>Welcom to Profile Page</h1>
+      <h1>Welcome to Profile Page</h1>
+      <p>
+        Welcome to your profile <b>{session.username}</b>
+      </p>
+      <span>
+        You are a{" "}
+        <b style={{ color: session.isPro ? "gold" : "silver" }}>
+          {session.isPro ? "Premium" : "Free"}
+        </b>{" "}
+        user
+      </span>
     </div>
   );
 };
