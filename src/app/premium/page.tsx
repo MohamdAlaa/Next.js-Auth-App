8b8648da4fdcf5deeeb1ef4e2e6582d021817296
MohamdAlaa <mohamedalaaelmenyawe606@gmail.com>
2025-08-11 14:13:56 +0300

    added loggic to Premiumpage for Premium users

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,7 +1,37 @@
-const Premiumpage = () => {
+import { getSession } from "@/actions";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+const Premiumpage = async () => {
+  const session = await getSession();
+  if (!session.isLoggedIn) {
+    redirect("/login");
+  }
+
+  if (!session.isPro) {
+    return (
+      <div className="notPremium">
+        <h1>Only Premium Users can see the content</h1>
+        <Link href="/profile">
+          <span>Go to profile page to upgrade to Premuim</span>
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className="premium">
       <h1>Welcome to Premium Page</h1>
+      <p>
+        This page is only accessible to users with a premium subscription. Enjoy
+        exclusive content and features!
+      </p>
+      <ul>
+        <li>Exclusive articles and tutorials</li>
+        <li>Ad-free browsing experience</li>
+        <li>Priority support</li>
+        <li>Access to premium resources</li>
+      </ul>
     </div>
   );
 };
